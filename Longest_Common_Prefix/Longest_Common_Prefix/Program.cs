a97cd6a5cf6de25001b112e1a34cd272e9aa0c8e
Mihaidono <amihai2001@gmail.com>
2023-06-11 01:03:59 +0300

    works

--- Longest_Common_Prefix/Longest_Common_Prefix/Program.cs
+++ Longest_Common_Prefix/Longest_Common_Prefix/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace Longest_Common_Prefix
+﻿using System;
+using System.Linq;
+
+namespace Longest_Common_Prefix
 {
     internal class Program
     {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length <= 0) return string.Empty;
+            var prefix = strs[0];
+            while (prefix.Length > 0)
+            {
+                if (strs.Where(elem => elem.Length >= prefix.Length && elem.Substring(0,prefix.Length) == prefix).ToArray().Length == strs.Length)
+                {
+                    return prefix;
+                }
+
+                prefix = prefix.Remove(prefix.Length - 1);
+            }
+
+            return string.Empty;
+        }
+
         public static void Main(string[] args)
         {
+            Console.WriteLine("Example 1:(output should be ' fl ')");
+            Console.WriteLine(LongestCommonPrefix(new[] { "flowers", "flow", "flight" }));
         }
     }
 }
